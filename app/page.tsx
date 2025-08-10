d52ee87c07fead5ea1f325f5b4ca124bb24de9c8
almrakb <almrakb2024@gmail.com>
2025-08-10 18:31:10 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -814,3 +814,26 @@ export default function Page() {
     </main>
   )
 }
+import Script from "next/script";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-780NLK51R0`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-780NLK51R0', { debug_mode: true });
+          `}
+        </Script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
